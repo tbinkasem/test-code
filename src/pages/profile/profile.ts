5e3cfc309e8399fc390832bcf98998f7ccf727b0
Teera Binkasem <bteera@gmail.com>
2018-09-12 17:31:17 +0700

    new 2 profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Profile } from '../../models/profiles';
+import { Profile } from '../../models/profile';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { HomePage } from '../../pages/home/home';
